3e9742b4789695daa4de81fde0cb3beb5f66b049
Anushka Gupta <anushkagupta319@gmail.com>
2024-07-01 12:04:48 +0530

    fc

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,37 @@
 const express = require('express') 
 const app = express()
-const port = 3000 // default port
+const port = process.env.PORT || 5001
+require('dotenv').config()
+
+
+//mongo db connection 
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://anushkagupta319:oCadP1RdsuxHDYJD@yoga.fujusb3.mongodb.net/?retryWrites=true&w=majority&appName=yoga`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => { // redirect to the root URL
   res.send('Hello World!')
@@ -9,3 +40,4 @@ app.get('/', (req, res) => { // redirect to the root URL
 app.listen(port, () => { // listen on port 3000
   console.log(`Example app listening on port ${port}`)
 })
+//oCadP1RdsuxHDYJD -> anushkagupta319
